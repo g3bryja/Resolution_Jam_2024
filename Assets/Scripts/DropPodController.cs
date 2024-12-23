497e4c11a0989b12bd851ead29fabac2ff3165fa
Gino Bryja <g3bryja@gmail.com>
2024-12-23 18:13:49 -0500

    Added Drop Pod as prefab, updates to controller and particle effects

--- Assets/Scripts/DropPodController.cs
+++ Assets/Scripts/DropPodController.cs
@@ -11,19 +11,37 @@ public class DropPodController : MonoBehaviour
     [SerializeField]
     private float moveAccelerationTime = 0.3f;
 
-    private Rigidbody rigidbody;
+    [SerializeField]
+    private float maxRotation = 15.0f;
 
+    [SerializeField]
+    private float rotationSmoothing = 7.0f;
+
+    [SerializeField]
+    private Transform model;
+
+    private int direction = 0;
     private int prevDirection = 0;
 
-    private void Awake() {
-        rigidbody = GetComponent<Rigidbody>();
+    private void Update() {
+        direction = (int)PlayerInput.instance.DirectionalInput.x;
+        Move();
+        Rotate();
+        prevDirection = direction;
     }
 
-    private void Update() {
-        int direction = (int)PlayerInput.instance.DirectionalInput.x;
-
+    private void Move() {
         transform.position += Vector3.right * moveSpeed * direction * Time.deltaTime;
+    }
 
-        prevDirection = direction;
+    private void Rotate() {
+        float targetRotation = 0;
+
+        if (direction != 0) {
+            targetRotation = maxRotation * Mathf.Sign(direction);
+        }
+
+        Quaternion target = Quaternion.Euler(Vector3.forward * targetRotation);
+        model.rotation = Quaternion.Slerp(model.rotation, target,  Time.deltaTime * rotationSmoothing);
     }
 }
